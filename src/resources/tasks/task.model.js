c3bad92d51d6853810ab761e4b687964eb3f1f67
Antowa-qq <anton-vinsss@mail.ru>
2020-10-12 01:41:48 +0300

    feat: add tasks model

---
+++ src/resources/tasks/task.model.js
@@ -0,0 +1,28 @@
+const uuid = require('uuid');
+
+class Task {
+  constructor({
+    id = uuid(),
+    title = '',
+    order = '',
+    description = '',
+    userId = '',
+    boardId = '',
+    columnId = ''
+  } = {}) {
+    this.id = id;
+    this.title = title;
+    this.order = order;
+    this.description = description;
+    this.userId = userId;
+    this.boardId = boardId;
+    this.columnId = columnId;
+  }
+
+  // static toResponse(user) {
+  //   const { id, name, login } = user;
+  //   return { id, name, login };
+  // }
+}
+
+module.exports = Task;
